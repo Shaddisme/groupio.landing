26013edee5bd4f63596811884887d5a9ec15a6a7
Sandy Abasman <abasifrekesandy@gmail.com>
2025-02-02 20:07:44 +0200

    first

---
+++ groupio Landing/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-primaryColors-primary_1_light">
+      <h1>app</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ groupio Landing/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryColors: {
+          primary_1_light: " #1E7B68",
+          primary_1: "#145245",
+          primary_2_Dark: "#0A2923",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
