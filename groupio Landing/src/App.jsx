68de6fb440af69162cf6be7bb9bf3a0bb741a2d9
Sunday Abasman <74963378+SandyAbasman@users.noreply.github.com>
2025-02-04 00:49:52 +0400

    Revert "Router"

--- groupio Landing/src/App.jsx
+++ groupio Landing/src/App.jsx
@@ -1,24 +1,9 @@
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router";
-import RootLayout from "./pages/RootLayout";
-import LandingPage from "./pages/LandingPage";
-import ContactPage from "./pages/ContactPage";
-
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<RootLayout />}>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-      </Route>
-    )
+  return (
+    <div className="bg-primaryColors-primary_1_light">
+      <h1>app</h1>
+    </div>
   );
-
-  return <RouterProvider router={router} />;
 }
 
 export default App;
--- groupio Landing/src/pages/LandingPage.jsx
+++ groupio Landing/src/pages/LandingPage.jsx
@@ -1,5 +1,11 @@
+import Header from "../components/Header";
+
 const LandingPage = () => {
-  return <div className="bg-red-600">homepage page</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default LandingPage;
