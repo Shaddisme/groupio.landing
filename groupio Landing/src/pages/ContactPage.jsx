75a748b4c68088cdfd798d159c703b2ed88199b5
Sandy Abasman <abasifrekesandy@gmail.com>
2025-02-03 22:53:20 +0200

    cleared main

--- groupio Landing/src/pages/ContactPage.jsx
+++
@@ -1,5 +0,0 @@
-function ContactPage() {
-  return <div className="bg-blue-600">ContactPage</div>;
-}
-
-export default ContactPage;
--- groupio Landing/src/pages/LandingPage.jsx
+++
@@ -1,5 +0,0 @@
-const LandingPage = () => {
-  return <div className="bg-red-600">homepage page</div>;
-};
-
-export default LandingPage;
