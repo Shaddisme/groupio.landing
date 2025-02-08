f61f1b5d604a4f362d3d8044729e43ff3ba3c066
Sandy Abasman <abasifrekesandy@gmail.com>
2025-02-08 18:15:21 +0200

    creating the header

--- groupio Landing/src/App.jsx
+++ groupio Landing/src/App.jsx
@@ -6,14 +6,12 @@ import {
 } from "react-router";
 import RootLayout from "./pages/RootLayout";
 import LandingPage from "./pages/LandingPage";
-import ContactPage from "./pages/ContactPage";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<RootLayout />}>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/contact" element={<ContactPage />} />
       </Route>
     )
   );
---
+++ groupio Landing/src/pages/Landingpage/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <div className="px-[6.25rem] py-[1rem]">
+      <div>
+        <img src="/logo" alt="logo" />
+      </div>
+
+      <div></div>
+    </div>
+  );
+}
+
+export default Header;
